6a5ca3687c5bfd30ca3c3834284edd63a9802f86
Ron Lisle <ron@lisles.net>
2017-05-20 09:54:10 -0500

    Update examples and add plugin examples

--- Photon/IoTlib/examples/starter/starter.ino
+++ Photon/IoTlib/examples/starter/starter.ino
@@ -1,8 +1,13 @@
 /******************************************************************
 Example starting point sketch for Patriot library
 
-This example uses the built-in blue LED only.
+This example uses the built-in blue LED and a switch on D0.
+You can use a jumper wire to connect D0 to ground to turn the
+LED on and off, or use the iOS app or Alexa.
+
 It is provided as a quick start for your own sketches.
+Alternatively, you can use the Everything example, and delete
+what you don't need.
 
 http://www.github.com/rlisle/Patriot
 
@@ -17,6 +22,9 @@ Changelog:
 ******************************************************************/
 
 #include <IoT.h>
+#include <PatriotLight.h>
+#include <PatriotSwitch.h>
+
 IoT *iot;
 
 void setup() {
@@ -24,13 +32,20 @@ void setup() {
     iot->setControllerName("myPhoton"); // The name of your Photon device. Can be whatever you named it.
     iot->begin();
 
+    // Create devices
     //Note: D7 is not a PWM pin, so it can only turn on or off
-    iot->addLight(D7, "Led");           // This is the small blue LED on a photon
+    Light *light1 = new Light(D7, "Led");
+    Switch *switch1 = new Switch(D0, "PushMe");
 
-    // Alexa will respond to "Alexa, turn photon on" or "Alexa, turn on photon"
+    // Tell IoT about the devices you defined above
+    iot->addDevice(light1);
+    iot->addDevice(switch1);
+
+    // The "behavior" defines the commands that control things.
+    // Alexa will respond to "Alexa, turn photon on" or "Alexa, turn off photon"
     // You can change the word 'photon' to whatever you like, but it needs to be something
     // that Alexa can recognize. For now, use a single word.
-    iot->addBehavior("Led", new Behavior("photon",'>',0, 100));
+    iot->addBehavior(new Behavior(light1, "photon", '>', 0, 100));
 }
 
 void loop() {
