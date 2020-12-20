a2de1b223b4ef8093043e928032cbc3134f55435
Ron <ron@lisles.net>
2020-12-20 08:48:52 -0600

    Convert FrontPanel

--- Photon/Controllers/FrontPanel/src/FrontPanel.ino
+++ Photon/Controllers/FrontPanel/src/FrontPanel.ino
@@ -44,64 +44,65 @@ String mqttServer = "192.168.10.184";
 
 IoT *iot;
 
-// Uncomment when wiring change complete
-NCD8Light dsFloods(ADDRESS2, 0, "DoorSide");
-NCD8Light kitchenCeiling(ADDRESS2, 1, "kitchenCeiling", 0);
-NCD8Light sink(ADDRESS2, 2, "Sink", 2);
-NCD8Light osFloods(ADDRESS2, 3, "OtherSide");
-NCD8Light rightTrim(ADDRESS2, 4, "RightTrim");
-NCD8Light leftTrim(ADDRESS2, 5, "LeftTrim");
-NCD8Light frontAwning(ADDRESS2, 6, "FrontAwning");
-NCD8Light frontPorch(ADDRESS2, 7, "FrontPorch");
-
-Light ceiling(D2, "ceiling", 2);
-Light cabinets(D3, "cabinets", 2);
               
-// Enable and reorder once wiring connected
-//Switch ceilingSwitch(A0, "CeilingSwitch");
-//Switch kitchenCeilingSwitch(A1, "KitchenCeilingSwitch");
-//Switch sinkSwitch(A2, "SinkSwitch");
-//Switch cabinetSwitch(A3, "CabinetSwitch");
-//Switch rightTrimSwitch(A4, "RightTrimSwitch");
-//Switch leftTrimSwitch(A5, "LeftTrimSwitch");
-//Switch dsFloodsSwitch(A6, "DSFloodsSwitch");
-//Switch osFloodsSwitch(A7, "ODSFloodsSwitch");
-//Switch frontPorchSwitch(RX, "FrontPorchSwitch");
-//Switch frontAwningSwitch(TX, "FrontAwningSwitch");
-
-// Activities allow Alexa to control them
-// and can also turn off other activities.
-// These will be used by other panels also, but don't need to be duplicated by them
-//Activity waking("waking");                  // Turns off sleeping
-
 void setup() {
     iot = IoT::getInstance();
     iot->setControllerName("FrontPanel");
     iot->begin();
     iot->connectMQTT(mqttServer, "patriotFrontPanel1");
 
+    NCD8Light *dsFloods = new NCD8Light(ADDRESS2, 0, "DoorSide");
+    NCD8Light *kitchenCeiling = new NCD8Light(ADDRESS2, 1, "kitchenCeiling", 0);
+    NCD8Light *sink = new NCD8Light(ADDRESS2, 2, "Sink", 2);
+    NCD8Light *osFloods = new NCD8Light(ADDRESS2, 3, "OtherSide");
+    NCD8Light *rightTrim = new NCD8Light(ADDRESS2, 4, "RightTrim");
+    NCD8Light *leftTrim = new NCD8Light(ADDRESS2, 5, "LeftTrim");
+    NCD8Light *frontAwning = new NCD8Light(ADDRESS2, 6, "FrontAwning");
+    NCD8Light *frontPorch = new NCD8Light(ADDRESS2, 7, "FrontPorch");
+
+    Light *ceiling = new Light(D2, "ceiling", 2);
+    Light *cabinets = new Light(D3, "cabinets", 2);
+
+    // Enable and reorder once wiring connected
+    //Switch *ceilingSwitch = new Switch(A0, "CeilingSwitch");
+    //Switch *kitchenCeilingSwitch = new Switch(A1, "KitchenCeilingSwitch");
+    //Switch *sinkSwitch = new Switch(A2, "SinkSwitch");
+    //Switch *cabinetSwitch = new Switch(A3, "CabinetSwitch");
+    //Switch *rightTrimSwitch = new Switch(A4, "RightTrimSwitch");
+    //Switch *leftTrimSwitch = new Switch(A5, "LeftTrimSwitch");
+    //Switch *dsFloodsSwitch = new Switch(A6, "DSFloodsSwitch");
+    //Switch *osFloodsSwitch = new Switch(A7, "ODSFloodsSwitch");
+    //Switch *frontPorchSwitch = new Switch(RX, "FrontPorchSwitch");
+    //Switch *frontAwningSwitch = new Switch(TX, "FrontAwningSwitch");
+
+    // Activities allow Alexa to control them
+    // and can also turn off other activities.
+    // Currently all activities are defined in RearPanel
+    //Activity waking = new Activity("waking");                  // Turns off sleeping
+
+
     // Set other states
-//    waking.setOtherState("sleeping", 0);        // Turn off sleeping when waking
+//    waking->setOtherState("sleeping", 0);        // Turn off sleeping when waking
 
     // BEHAVIORS
 
     // Waking
-    ceiling.addBehavior(30, "waking", '>', 0);
-    kitchenCeiling.addBehavior(30, "waking", '>', 0);
-    cabinets.addBehavior(30, "waking", '>', 0);
-    sink.addBehavior(30, "waking", '>', 0);
+    ceiling->addBehavior(30, "waking", '>', 0);
+    kitchenCeiling->addBehavior(30, "waking", '>', 0);
+    cabinets->addBehavior(30, "waking", '>', 0);
+    sink->addBehavior(30, "waking", '>', 0);
     
     // Watching
-    ceiling.addBehavior(70, "watching", '>', 0);
-    kitchenCeiling.addBehavior(50, "watching", '>', 0);
-    cabinets.addBehavior(50, "watching", '>', 0);
-    sink.addBehavior(30, "watching", '>', 0);
+    ceiling->addBehavior(70, "watching", '>', 0);
+    kitchenCeiling->addBehavior(50, "watching", '>', 0);
+    cabinets->addBehavior(50, "watching", '>', 0);
+    sink->addBehavior(30, "watching", '>', 0);
 
     // Retiring
-    ceiling.addBehavior(30, "retiring", '>', 0);
-    kitchenCeiling.addBehavior(30, "retiring", '>', 0);
-    cabinets.addBehavior(30, "retiring", '>', 0);
-    sink.addBehavior(30, "retiring", '>', 0);
+    ceiling->addBehavior(30, "retiring", '>', 0);
+    kitchenCeiling->addBehavior(30, "retiring", '>', 0);
+    cabinets->addBehavior(30, "retiring", '>', 0);
+    sink->addBehavior(30, "retiring", '>', 0);
 
     // Sleeping
     // Handled by Alexa room commands
@@ -109,41 +110,41 @@ void setup() {
     
     // Switches
 // Uncomment these once they're hooked up. Otherwise they appear to be ON
-//    ceiling.addBehavior(100, "CeilingSwitch", '>', 0);
-//    kitchenCeiling.addBehavior(100, "KitchenCeilingSwitch", '>', 0);
-//    sink.addBehavior(100, "SinkSwitch", '>', 0);
-//    kitchenCabinets.addBehavior(100, "CabinetSwitch", '>', 0);
-//    rightTrim.addBehavior(100, "RightTrimSwitch", '>', 0);
-//    leftTrim.addBehavior(100, "LeftTrimSwitch", '>', 0);
-//    dsFloods.addBehavior(100, "DSFloodsSwitch", '>', 0);
-//    osFloods.addBehavior(100, "OSFloodsSwitch", '>', 0);
-//    frontPorch.addBehavior(100, "FrontPorchSwitch", '>', 0);
-//    frontAwning.addBehavior(100, "FrontAwningSwitch", '>', 0);
+//    ceiling->addBehavior(100, "CeilingSwitch", '>', 0);
+//    kitchenCeiling->addBehavior(100, "KitchenCeilingSwitch", '>', 0);
+//    sink->addBehavior(100, "SinkSwitch", '>', 0);
+//    kitchenCabinets->addBehavior(100, "CabinetSwitch", '>', 0);
+//    rightTrim->addBehavior(100, "RightTrimSwitch", '>', 0);
+//    leftTrim->addBehavior(100, "LeftTrimSwitch", '>', 0);
+//    dsFloods->addBehavior(100, "DSFloodsSwitch", '>', 0);
+//    osFloods->addBehavior(100, "OSFloodsSwitch", '>', 0);
+//    frontPorch->addBehavior(100, "FrontPorchSwitch", '>', 0);
+//    frontAwning->addBehavior(100, "FrontAwningSwitch", '>', 0);
 
     // DEVICES
 
-    iot->addDevice(&kitchenCeiling);
-    iot->addDevice(&sink);
-    iot->addDevice(&rightTrim);
-    iot->addDevice(&leftTrim);
-    iot->addDevice(&dsFloods);
-    iot->addDevice(&osFloods);
-    iot->addDevice(&frontAwning);
-    iot->addDevice(&frontPorch);
+    iot->addDevice(kitchenCeiling);
+    iot->addDevice(sink);
+    iot->addDevice(rightTrim);
+    iot->addDevice(leftTrim);
+    iot->addDevice(dsFloods);
+    iot->addDevice(osFloods);
+    iot->addDevice(frontAwning);
+    iot->addDevice(frontPorch);
 
-    iot->addDevice(&ceiling);
-    iot->addDevice(&cabinets);
+    iot->addDevice(ceiling);
+    iot->addDevice(cabinets);
 
-//    iot->addDevice(&ceilingSwitch);
-//    iot->addDevice(&kitchenCeilingSwitch);
-//    iot->addDevice(&sinkSwitch);
-//    iot->addDevice(&cabinetSwitch);
-//    iot->addDevice(&rightTrimSwitch);
-//    iot->addDevice(&leftTrimSwitch);
-//    iot->addDevice(&dsFloodsSwitch);
-//    iot->addDevice(&osFloodsSwitch);
-//    iot->addDevice(&frontPorchSwitch);
-//    iot->addDevice(&frontAwningSwitch);
+//    iot->addDevice(ceilingSwitch);
+//    iot->addDevice(kitchenCeilingSwitch);
+//    iot->addDevice(sinkSwitch);
+//    iot->addDevice(cabinetSwitch);
+//    iot->addDevice(rightTrimSwitch);
+//    iot->addDevice(leftTrimSwitch);
+//    iot->addDevice(dsFloodsSwitch);
+//    iot->addDevice(osFloodsSwitch);
+//    iot->addDevice(frontPorchSwitch);
+//    iot->addDevice(frontAwningSwitch);
 
 }
 
