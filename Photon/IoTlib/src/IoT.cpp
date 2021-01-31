0c2031ca6b58601695c6c454eeecef6980afe0cc
Ron <ron@lisles.net>
2021-01-31 07:00:20 -0600

    cleanup

--- Photon/IoTlib/src/IoT.cpp
+++ Photon/IoTlib/src/IoT.cpp
@@ -73,28 +73,6 @@ void IoT::subscribeHandler(const char *eventName, const char *rawData)
     if (_mqttManager != NULL) {
         _mqttManager->parseMessage(event.toLowerCase(), data.toLowerCase());
     }
-//    if(event.equalsIgnoreCase(kPublishName) == false) {
-//        Log.warn("IoT received unexpected particle.io topic: " + event);
-//        return;
-//    }
-//
-//    // Legacy commands will include a colon
-//    // t:patriot m:<eventName>:<msg>
-//    int colonPosition = data.indexOf(':');
-//    if(colonPosition == -1) {
-//        Log.error("IoT received invalid particle message: " + data);
-//        return;
-//    }
-//
-//    Log.info("Particle.io subscribe received data: '"+data+"'");
-//
-//    String name = data.substring(0,colonPosition).toLowerCase();
-//    String level = data.substring(colonPosition+1).toLowerCase();
-//    String topic = kPublishName + "/" + name;
-//
-//      if (_mqttManager != NULL) {
-//          _mqttManager->parseMessage(topic, level);
-//      }
 }
 
 /**
