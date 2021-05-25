c1ff6bce4a2421a96478bd1985a508bac1f10304
Ron <ron@lisles.net>
2021-05-25 06:44:02 -0500

    Add SwiftUI project

---
+++ iOS/Patriot/Patriot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Patriot
+//
+//  Created by Ron Lisle on 5/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
