42e2edf3a29e381bb385ff5ccbe5a3569ec630a9
Ron <ron@lisles.net>
2021-07-02 21:54:59 -0500

    New project

---
+++ iOS/Patriot3/Patriot3/Patriot3App.swift
@@ -0,0 +1,17 @@
+//
+//  Patriot3App.swift
+//  Patriot3
+//
+//  Created by Ron Lisle on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Patriot3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
