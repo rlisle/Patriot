e76cab892b54a6286e1b89a7c92adf9c60f5c644
Ron <ron@lisles.net>
2021-12-21 09:43:36 -0600

    wip

--- Photon/Controllers/RearPanel/src/RearPanel.ino
+++ Photon/Controllers/RearPanel/src/RearPanel.ino
@@ -54,7 +54,7 @@ void createDevices() {
     Device::add(new NCD4Switch(I2CR4IO4, 0, "OfficeDoor", "Office"));
     
     // Photon I/O
-    Device::add(new PIR(A5, "OfficeMotion"));
+    Device::add(new PIR(A5, "OfficeMotion", "Office"));
 
     // I2CPWM8W80C board
     // 8 Dimmers
