30f2b0863fb081879a7099e91683c46079bdb0d1
Ron <ron@lisles.net>
2022-01-08 06:31:47 -0600

    wip

--- Photon/Plugins/NCD4Relay/src/PatriotNCD4Relay.cpp
+++ Photon/Plugins/NCD4Relay/src/PatriotNCD4Relay.cpp
@@ -23,50 +23,38 @@
 #include "PatriotNCD4Relay.h"
 
 #define MILLIS_PER_SECOND 1000
+#define REGISTER_ADDRESS 0x0A
 
-int8_t NCD4Relay::_numControllers = 0;  // Count of relay boards on I2C bus
-int8_t NCD4Relay::_currentStates[4];    // All relays initially off
-int8_t NCD4Relay::_addresses[4];        // Addresses of up to 8 boards
+int8_t NCD4Relay::_currentState;   // Bitmap of relay states
+int8_t NCD4Relay::_address;        // Addresses of board
 
 /**
  * Constructor
  * @param address is the board address set by jumpers (0-7)
  * @param relayNum is the relay number on the NCD 4 Relay board (1-4)
  * @param name String name used to address the relay.
- * @param duration Optional seconds value to automatically turn off relay. 0 = no automatic turn off.
+ * @param room String name of location room
  */
 NCD4Relay::NCD8Relay(int8_t address, int8_t relayNum, String name, String room)
     : Device(name, room)
 {
-    _boardAddress = address;   // 0x20 (no jumpers)
+    _address    = address;   // 0x20 (no jumpers)
     _relayNum   = relayNum;
     _percent    = 0;
-    _stopMillis = 0;
     _type       = 'R';
 }
 
 void NCD4Relay::begin() {
 
-    _boardIndex = initialize4RelayBoard(address);
+    initializeBoard();
     
     // Initialize relay to off
     _percent = 55;  // Just to allow setOff to not ignore
     setOff();
 }
 
-int8_t NCD4Relay::initialize4RelayBoard(int8_t address) {
-
-    _registerAddress = 0x0A;
-
-    int8_t index = boardIndex(address);
-    if(index < 0)
-    {
-        index = initializeBoard(address);
-    }
-    return index;
-}
-
-int8_t NCD8Relay::initializeBoard(int8_t address) {
+void NCD4Relay::initializeBoard() {
+    
     int retries;
     byte status;
 
@@ -77,47 +65,26 @@ int8_t NCD8Relay::initializeBoard(int8_t address) {
 
     retries = 0;
     do {
-        Wire.beginTransmission(address);
+        Wire.beginTransmission(_address);
         Wire.write(0x00);                   // Select IO Direction register
         Wire.write(0x00);                   // Set all 8 to outputs
         status = Wire.endTransmission();    // ??? Write 'em, Dano
+    } while( status != 0 && retries++ < 3);
+    if(status != 0) {
+        Log.error("Set IODIR failed");
+    }
 
-        // Note: pull-ups should have no effect on outputs, so this looks wrong.
+    retries = 0;
+    do {
+        Wire.beginTransmission(_address);
         Wire.write(0x06);                   // ??? Select pull-up resistor register
         Wire.write(0x00);                   // ??? pull-ups disabled on all 8 outputs
         status = Wire.endTransmission();
     } while( status != 0 && retries++ < 3);
-
     if(status != 0) {
-        Log.error("Set address failed");
+        Log.error("Set GPPU failed");
         //TODO: handle error
     }
-
-    return addAddressToArray(address);
-}
-
-int8_t NCD8Relay::boardIndex(int8_t address) {
-    for(int8_t index=0; index<_numControllers; index++) {
-        if(_addresses[index] == address) {
-            return index;
-        }
-    }
-    return -1;
-}
-
-int8_t NCD8Relay::addAddressToArray(int8_t address) {
-    //_currentStates[_numControllers] = 0x00; Leave as set in SRAM
-    _addresses[_numControllers] = address;
-
-    // // This looks wrong. Probably delete remainder of code here.
-    // Wire.write(_registerAddress);
-    // Wire.write(0x00);                      // Turn off all relays
-    // byte status = Wire.endTransmission();
-    // if(status != 0) {
-    //     //TODO: handle any errors, retry, etc.
-    //     Log.error("Error turning off relays");
-    // }
-    return _numControllers++;
 }
 
 /**
@@ -125,7 +92,7 @@ int8_t NCD8Relay::addAddressToArray(int8_t address) {
  * This is how things are turned on/off in Patriot
  * @param percent Int 0 to 100. 0 = off, >0 = on
  */
-void NCD8Relay::setPercent(int percent) {
+void NCD4Relay::setPercent(int percent) {
     if(percent == 0) setOff();
     else setOn();
 }
@@ -133,26 +100,19 @@ void NCD8Relay::setPercent(int percent) {
 /**
  * Set On
  */
-void NCD8Relay::setOn() {
-
-    if(_percent > 0) return;
-
+void NCD4Relay::setOn() {
+    
     _percent = 100;
 
-    if(_duration != 0)
-    {
-        _stopMillis = millis() + (_duration * MILLIS_PER_SECOND);
-    }
-
     byte bitmap = 1 << _relayNum;
-    NCD8Relay::_currentStates[_boardIndex] |= bitmap;            // Set relay's bit
+    NCD4Relay::_currentState |= bitmap;            // Set relay's bit
 
     byte status;
     int retries = 0;
     do {
-        Wire.beginTransmission(_addresses[_boardIndex]);
-        Wire.write(_registerAddress);
-        Wire.write(NCD8Relay::_currentStates[_boardIndex]);
+        Wire.beginTransmission(_address);
+        Wire.write(REGISTER_ADDRESS);
+        Wire.write(NCD8Relay::_currentState);
         status = Wire.endTransmission();
     } while(status != 0 && retries++ < 3);
 
@@ -166,21 +126,19 @@ void NCD8Relay::setOn() {
  * Set relay off
  */
 void NCD8Relay::setOff() {
-    if(_percent == 0) return;
-
+    
     _percent = 0;
-    _stopMillis = 0;
 
     byte bitmap = 1 << _relayNum;
     bitmap = 0xff ^ bitmap;
-    NCD8Relay::_currentStates[_boardIndex] &= bitmap;
+    NCD8Relay::_currentState &= bitmap;
 
     byte status;
     int retries = 0;
     do {
-        Wire.beginTransmission(_addresses[_boardIndex]);
-        Wire.write(_registerAddress);
-        Wire.write(NCD8Relay::_currentStates[_boardIndex]);
+        Wire.beginTransmission(_address);
+        Wire.write(REGISTER_ADDRESS);
+        Wire.write(NCD8Relay::_currentState);
         byte status = Wire.endTransmission();
     } while(status != 0 && retries++ < 3);
 
@@ -199,12 +157,5 @@ void NCD8Relay::setOff() {
  */
 void NCD8Relay::loop()
 {
-    if(_stopMillis != 0)
-    {
-        if(millis() >= _stopMillis)
-        {
-            _stopMillis = 0;
-            setOff();
-        }
-    }
+    // Nothing to do
 };
--- Photon/Plugins/NCD4Relay/src/PatriotNCD4Relay.h
+++ Photon/Plugins/NCD4Relay/src/PatriotNCD4Relay.h
@@ -1,10 +1,9 @@
 /**
- NCD Relay control
-
+ NCD 4 Relay control
+ 
  Features:
+ - Supports up to 4 relays on 4 R 4 GPIO board
  - On/Off control
- - Supports multiple boards
- - Automatic shut off if duration specified
 
  http://www.github.com/rlisle/Patriot
 
@@ -16,8 +15,7 @@
  Datasheets:
 
  Changelog:
- 2018-01-05: v3 Assume use of Backup SRAM
- 2017-10-03: Initial creation
+ 2022-01-08: Initial creation based on curtian and NCD8Relay plugins
 */
 
 #pragma once
@@ -25,27 +23,17 @@
 #include "Particle.h"
 #include <device.h>
 
-class NCD8Relay : public Device
+class NCD4Relay : public Device
 {
  private:
-    int8_t  _relayNum;
-    int8_t  _boardIndex;            // Index into static arrays
-    int8_t  _registerAddress;       // Is this different for different boards?
-    int8_t  _duration;              // # seconds to keep on. 0 = forever
-    int8_t  _numRelays;             // # of relays on board (8 or less)
-    long    _stopMillis;            // time in millis to auto-stop
+    int8_t        _relayNum;
+    static int8_t _currentState;
+    static int8_t _address;           // Addresses of board
 
-    static int8_t _numControllers;    // Count of relay boards on I2C bus
-    static int8_t _currentStates[];   // up to 8 relays currently supported
-    static int8_t _addresses[];       // Addresses of up to 8 boards
-
-    int8_t  initialize8RelayBoard(int8_t address);
-    int8_t  initializeBoard(int8_t address);
-    int8_t  boardIndex(int8_t address);
-    int8_t  addAddressToArray(int8_t address);
+    void   initializeBoard();
 
  public:
-    NCD8Relay(int8_t address, int8_t numRelays, int8_t relayNum, String name, String room, int8_t duration = 0);
+    NCD4Relay(int8_t address, int8_t relayNum, String name, String room);
     
     void    begin();
 
