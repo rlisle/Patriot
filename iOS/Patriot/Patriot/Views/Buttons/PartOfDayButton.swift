ce429dc5a167edbe594541824ee308d5d678ae08
Ron <ron@lisles.net>
2022-05-11 06:54:08 -0500

    wip

--- iOS/Patriot/Patriot/Views/Buttons/PartOfDayButton.swift
+++ iOS/Patriot/Patriot/Views/Buttons/PartOfDayButton.swift
@@ -17,19 +17,87 @@ struct PartOfDayButton: View {
                 print("PartOfDay")
             }
         }) {
-            Image(systemName: "sun.and.horizon")
-                .imageScale(.large)
+            PartOfDayView()
         }.foregroundColor(.white)
+    }
+}
+
+struct PartOfDayView: View {
+
+    @EnvironmentObject var model: PatriotModel
+    
+    var body: some View {
+        Image(systemName: partOfDayIcon())
+            .imageScale(.large)
+    }
+    
+    func partOfDayIcon() -> String {
+        switch model.partOfDay {
+        case .Night:
+            return "moon.stars.fill"
+        case .Dawn, .Dusk:
+            return "moon.fill"
+        case .Sunrise, .Sunset:
+            return "sun.and.horizon"
+        case .Noon:
+            return "sun.max"
+        default:
+            return "sun.min"
+        }
     }
 }
 
 struct PartOfDayButton_Previews: PreviewProvider {
     static var previews: some View {
-        PartOfDayButton()
-            .environmentObject(PatriotModel(forTest: true))
-            .previewLayout(.sizeThatFits)
-            .padding()
-            .previewDisplayName("PartOfDay Button")
-            .background(.black)
+        Group {
+            PartOfDayButton()
+                .environmentObject(PatriotModel(forTest: true, partOfDay: .Night))
+                .previewLayout(.sizeThatFits)
+                .padding()
+                .previewDisplayName("Night")
+                .background(.black)
+
+            PartOfDayButton()
+                .environmentObject(PatriotModel(forTest: true, partOfDay: .Sunrise))
+                .previewLayout(.sizeThatFits)
+                .padding()
+                .previewDisplayName("Sunrise")
+                .background(.black)
+
+            PartOfDayButton()
+                .environmentObject(PatriotModel(forTest: true, partOfDay: .Morning))
+                .previewLayout(.sizeThatFits)
+                .padding()
+                .previewDisplayName("Morning")
+                .background(.black)
+
+            PartOfDayButton()
+                .environmentObject(PatriotModel(forTest: true, partOfDay: .Noon))
+                .previewLayout(.sizeThatFits)
+                .padding()
+                .previewDisplayName("Noon")
+                .background(.black)
+
+            PartOfDayButton()
+                .environmentObject(PatriotModel(forTest: true, partOfDay: .Afternoon))
+                .previewLayout(.sizeThatFits)
+                .padding()
+                .previewDisplayName("Afternoon")
+                .background(.black)
+
+            PartOfDayButton()
+                .environmentObject(PatriotModel(forTest: true, partOfDay: .Sunset))
+                .previewLayout(.sizeThatFits)
+                .padding()
+                .previewDisplayName("Sunset")
+                .background(.black)
+
+            PartOfDayButton()
+                .environmentObject(PatriotModel(forTest: true, partOfDay: .Dusk))
+                .previewLayout(.sizeThatFits)
+                .padding()
+                .previewDisplayName("Dusk")
+                .background(.black)
+        }
     }
 }
