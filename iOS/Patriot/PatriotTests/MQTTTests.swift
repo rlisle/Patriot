da01785e38c6c65698ce4f43ad39ddd2b2333855
Ron <ron@lisles.net>
2022-05-21 15:37:08 -0500

    wip

--- iOS/Patriot/PatriotTests/MQTTTests.swift
+++ iOS/Patriot/PatriotTests/MQTTTests.swift
@@ -11,12 +11,31 @@ import XCTest
 class MQTTTests: XCTestCase {
 
     var mqtt: MQTTManager!
+    var mqttHandler: MQTTHandler!
     
     override func setUpWithError() throws {
+        mqttHandler = MQTTHandler()
         mqtt = MQTTManager(forTest: true)
+        mqtt.mqttDelegate = mqttHandler
     }
 
     func testInstantiation() throws {
         XCTAssertNotNil(mqtt)
     }
+}
+
+class MQTTHandler: MQTTReceiving {
+    
+    var receivedTopic: String = ""
+    var receivedMessage: String = ""
+    
+    func connectionDidChange(isConnected: Bool) {
+        
+    }
+    
+    func didReceiveMessage(topic: String, message: String) {
+        receivedMessage = message
+        receivedTopic = topic
+    }
+
 }
