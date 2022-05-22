60fe4f11697d1ec428ddb62b26dd9dc9f271bc0a
Ron <ron@lisles.net>
2022-05-22 06:33:09 -0500

    wip

--- iOS/Patriot/Patriot/SceneDelegate.swift
+++ iOS/Patriot/Patriot/SceneDelegate.swift
@@ -23,34 +23,37 @@ class SceneDelegate: NSObject, UIWindowSceneDelegate, ObservableObject {
         print("sceneWillResignActive")
     }
     
-//    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
-//        print("scene continue userActivity")
-//        handleUserActivity(userActivity)
-//    }
-//
-//    func handleUserActivity(_ userActivity: NSUserActivity) {
-//
-//        print("Scene handleUserActivity")
-//
-//        guard let interaction = userActivity.interaction else {
-//            print("Scene interaction not set")
-//            return
-//        }
-//        guard let window = window else {
-//            print("Scene window not set")
-//            return
-//        }
-//        guard let rootViewController = window.rootViewController as? UINavigationController else {
-//            print("Scene rootViewController not set")
-//            return
-//        }
-//
-//        print("Scene TODO: handle it")
-//        if let intent = interaction.intent as? ShowDirectionsIntent,
-//           let recipe = intent.recipe,
-//           let viewController = rootViewController.viewControllers.last as? NextStepProviding {
-//            viewController.nextStep(recipe: recipe)
-//        }
-//    }
+    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+        print("scene continue userActivity")
+        handleUserActivity(userActivity)
+    }
+
+    func handleUserActivity(_ userActivity: NSUserActivity) {
+
+        print("Scene handleUserActivity")
+
+        guard let interaction = userActivity.interaction else {
+            print("Scene interaction not set")
+            return
+        }
+        guard let window = window else {
+            print("Scene window not set")
+            return
+        }
+        guard let rootViewController = window.rootViewController as? UINavigationController else {
+            print("Scene rootViewController not set")
+            return
+        }
+
+        print("Scene TODO: handle it")
+        if let intent = interaction.intent as? RVIntent,
+           let device = intent.device,
+           let percent = intent.percent {
+               //TODO:
+               print("Scene TODO: device=\(device), percent=\(percent)")
+           //let viewController = rootViewController.viewControllers.last as? NextStepProviding {
+           //  viewController.nextStep(recipe: recipe)
+        }
+    }
 
 }
