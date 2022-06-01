cd04600e96a19af2d3ef673304c4afdbfb721467
Ron <ron@lisles.net>
2022-06-01 07:59:31 -0500

    wip

--- iOS/Patriot/PatriotUITests/PatriotUITests.swift
+++ iOS/Patriot/PatriotUITests/PatriotUITests.swift
@@ -2,6 +2,9 @@
 //  PatriotUITests.swift
 //  PatriotUITests
 //
+//  XCUITests are used to verify navigation through the app.
+//  UI appearance is tested using snapshot-testing in XCTests, not UI tests.
+//
 //  Created by Ron Lisle on 6/1/22.
 //
 
@@ -17,83 +20,30 @@ class PatriotUITests: XCTestCase {
         app.launch()
     }
 
-//    func testRecord() throws {
-//
-//        let app = XCUIApplication()
-//        app.navigationBars["Patriot"].staticTexts["Patriot"].tap()
-//
-//        let scrollViewsQuery = app.scrollViews
-//        let bedroomStaticText = scrollViewsQuery.otherElements.containing(.staticText, identifier:"Kitchen").children(matching: .staticText)["Bedroom"]
-//        bedroomStaticText.tap()
-//        scrollViewsQuery.otherElements.staticTexts["Kitchen"].tap()
-//        bedroomStaticText.tap()
-//
-//    }
-
-    func testHomeScreenLaunches() {
+    func testAppNavigation() {
+        // Launch app
         let homeTitle = app.navigationBars["Patriot"].staticTexts["Patriot"]
         XCTAssert(homeTitle.waitForExistence(timeout: 3))
-    }
-    
-    func testSideMenu() {
-        let homeTitle = app.navigationBars["Patriot"].staticTexts["Patriot"]
-        XCTAssert(homeTitle.waitForExistence(timeout: 3))
+                
+        // Side Menu
         let patriotNavigationBar = app.navigationBars["Patriot"]
         let menuButton = patriotNavigationBar.buttons["Drag"]   // Is this the menu button? We should rename it
-        menuButton.tap()
-        XCTAssert(app.buttons["Awake"].waitForExistence(timeout: 3))
-    }
-    
-    func testDetailView() {
-        let homeTitle = app.navigationBars["Patriot"].staticTexts["Patriot"]
-        homeTitle.waitForExistence(timeout: 3)
+        menuButton.tap()    // Open side menu
+        XCTAssert(app.buttons["Awake"].waitForExistence(timeout: 3))    // Note: this may pass even with menu closed ???
+        menuButton.tap()    // Close side menu
+
+        // Device Details
         let scrollViewsQuery = app.scrollViews
         let kitchenElementsQuery = scrollViewsQuery.otherElements.containing(.staticText, identifier:"Kitchen")
         let lightoffImage = kitchenElementsQuery.children(matching: .other).element(boundBy: 1).children(matching: .image).matching(identifier: "LightOff").element(boundBy: 1)
         lightoffImage/*@START_MENU_TOKEN@*/.press(forDuration: 1.4);/*[[".tap()",".press(forDuration: 1.4);"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        
         XCTAssert(app.staticTexts["Room: Kitchen"].exists)
         XCTAssert(app.staticTexts["Sink"].exists)
+        
+        app.navigationBars["_TtGC7SwiftUI19UIHosting"].buttons["Patriot"].tap()
+
+        //TODO: how to test that tapping on a device turns it on/off?
+        
     }
-    
-//    func testPatriot() throws {
-//        let app = XCUIApplication()
-//        app.launch()
-//        let patriotNavigationBar = app.navigationBars["Patriot"]
-//        let dragButton = patriotNavigationBar.buttons["Drag"]   // Is this the menu button?
-//
-//        // Wait for app to launch
-//
-//        // Verify Home screen is displayed
-//
-//        // Navigate to side Menu
-//
-//        // Verify side Menu is displayed
-//
-//        // Return to Home screen
-//
-//        // Navigate to Device Detail screen
-//
-//
-//        dragButton.tap()
-//        app.buttons["Awake"].tap()
-//        dragButton.tap()
-//
-//        let scrollViewsQuery = app.scrollViews
-//        let kitchenElementsQuery = scrollViewsQuery.otherElements.containing(.staticText, identifier:"Kitchen")
-//        let lightoffImage = kitchenElementsQuery.children(matching: .other).element(boundBy: 1).children(matching: .image).matching(identifier: "LightOff").element(boundBy: 1)
-//        lightoffImage.tap()
-//
-//        let elementsQuery = scrollViewsQuery.otherElements
-//        elementsQuery.images["LightOn"].tap()
-//        lightoffImage/*@START_MENU_TOKEN@*/.press(forDuration: 1.4);/*[[".tap()",".press(forDuration: 1.4);"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-//        app.staticTexts["Room: Kitchen"].tap()
-//        app.staticTexts["Sink"].tap()
-//        app.navigationBars["_TtGC7SwiftUI19UIHosting"].buttons["Patriot"].tap()
-//        patriotNavigationBar.buttons["Walk"].tap()
-//        patriotNavigationBar.buttons["Brightness Lower"].tap()
-//        kitchenElementsQuery.children(matching: .staticText)["Bedroom"].tap()
-//        elementsQuery.staticTexts["Kitchen"].tap()
-//
-//
-//    }
 }
