ad99f3be59b0fa90b8f08052b3e7295ef1818393
Ron <ron@lisles.net>
2022-06-01 06:46:23 -0500

    Add menu tests

---
+++ iOS/Patriot/PatriotTests/MenuTests.swift
@@ -0,0 +1,28 @@
+//
+//  MenuViewTests.swift
+//  PatriotTests
+//
+//  Created by Ron Lisle on 6/1/22.
+//
+
+
+import XCTest
+import SwiftUI
+import SnapshotTesting
+@testable import Patriot
+
+class MenuViewTests: XCTestCase {
+    
+    var viewController: UIViewController!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        let modelView = PatriotModel(forTest: true)
+        let menuView = MenuView().environmentObject(modelView)
+        viewController = UIHostingController(rootView: menuView)
+    }
+    
+    func testMenuView() throws {
+        assertSnapshot(matching: viewController, as: .image(on: .iPhoneX), record: false)
+    }
+}
