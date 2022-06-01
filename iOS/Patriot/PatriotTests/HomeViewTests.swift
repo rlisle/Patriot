afd42fcfa63154f73d6d1fbdf75dc6d4542a1bf4
Ron <ron@lisles.net>
2022-06-01 05:33:35 -0500

    Remove UI testing target (for now)

--- iOS/Patriot/PatriotTests/HomeViewTests.swift
+++ iOS/Patriot/PatriotTests/HomeViewTests.swift
@@ -8,17 +8,17 @@
 
 import XCTest
 import SwiftUI
-@testable import Patriot
 import SnapshotTesting
+@testable import Patriot
 
 class HomeViewTests: XCTestCase {
     
-  let modelView = PatriotModel(forTest: true)
-
-  func testHomeView() throws {
-    let homeView = HomeView().environmentObject(PatriotModel(forTest: true))
-
-    let view: UIView = UIHostingController(rootView: homeView).view
-    assertSnapshot(matching: view, as: .image(size: view.intrinsicContentSize))
-  }
+//  let modelView = PatriotModel(forTest: true)
+//
+//  func testHomeView() throws {
+//    let homeView = HomeView() //.environmentObject(PatriotModel(forTest: true))
+//
+//    let view: UIView = UIHostingController(rootView: homeView).view
+//    assertSnapshot(matching: view, as: .image(size: view.intrinsicContentSize))
+//  }
 }
