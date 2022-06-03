a037039749a42ba8ed5d275ff93cb7b3a2bcfeb2
Ron <ron@lisles.net>
2022-06-03 06:24:03 -0500

    cleanup

--- iOS/Patriot/PatriotTests/PatriotModelTests.swift
+++ iOS/Patriot/PatriotTests/PatriotModelTests.swift
@@ -8,12 +8,66 @@
 import XCTest
 @testable import Patriot
 
+class MockSettingsStore: SettingsProviding {
+    var mockBool: Bool?
+    var mockInt: Int?
+    var mockString: String?
+    var mockStringArray: [String] = []
+    var mockDeviceArray: [Device] = []
+    
+    func getBool(forKey: SettingsKey) -> Bool? {
+        return mockBool
+    }
+    
+    func set(_ bool: Bool?, forKey: SettingsKey) {
+        mockBool = bool
+    }
+    
+    func getInt(forKey: SettingsKey) -> Int? {
+        return mockInt
+    }
+    
+    func set(_ int: Int?, forKey: SettingsKey) {
+        mockInt = int
+    }
+    
+    func getString(forKey: SettingsKey) -> String? {
+        return mockString
+    }
+    
+    func set(_ string: String?, forKey: SettingsKey) {
+        mockString = string
+    }
+    
+    func getStringArray(forKey: SettingsKey) -> [String] {
+        return mockStringArray
+    }
+    
+    func set(_ array: [String], forKey: SettingsKey) {
+        mockStringArray = array
+    }
+    
+    func getDeviceArray(forKey: SettingsKey) -> [Device] {
+        return mockDeviceArray
+    }
+    
+    func set(_ array: [Device], forKey: SettingsKey) {
+        mockDeviceArray = array
+    }
+    
+    
+}
+
 class PatriotModelTests: XCTestCase {
 
     var model: PatriotModel!
+    var store: SettingsProviding!
+    var settings: Settings!
     
     override func setUpWithError() throws {
         model = PatriotModel(forTest: true)
+        store = MockSettingsStore()
+        settings = Settings(store: store)
     }
 
     
@@ -26,8 +80,7 @@ class PatriotModelTests: XCTestCase {
     }
     
     func testUpdateFavoritesList() {
-        model.updateFavoritesList()
+        model.updateFavoritesList(settings: settings)
         XCTAssertEqual(model.favoritesList.count, 2)
     }
-
 }
