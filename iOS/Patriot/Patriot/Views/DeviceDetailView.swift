affbc196bc42627c5ac17d643c20ff1b7a8ff25a
Ron <ron@lisles.net>
2022-06-07 07:43:08 -0500

    wip

--- iOS/Patriot/Patriot/Views/DeviceDetailView.swift
+++ iOS/Patriot/Patriot/Views/DeviceDetailView.swift
@@ -7,22 +7,44 @@
 
 import SwiftUI
 
+struct PercentSlider: View {
+    @State var percent: Int = 0
+    var intProxy: Binding<Double>{
+        Binding<Double>(get: {
+            return Double(percent)
+        }, set: {
+            //rounds the double to an Int
+            print($0.description)
+            percent = Int($0)
+        })
+    }
+    var body: some View {
+        VStack{
+            Text(percent.description)
+            Slider(value: intProxy , in: 0.0...100.0, step: 5.0, onEditingChanged: {_ in
+                print(percent.description)
+            })
+        }
+    }
+}
+
 struct DeviceDetailView: View {
 
     @EnvironmentObject var model: PatriotModel
+    
+    @State var isEditing: Bool = false
     
     var body: some View {
         NavigationView {
             ScrollView {
                 //TODO: add more stuff'
                 VStack {
-                    Text("Room: \(model.selectedDevice.room)")
-                    Text(model.selectedDevice.name)
+                    PercentSlider(percent: model.selectedDevice.percent)
                 }
             }
             .frame(maxWidth: .infinity)
             .background(Color("BackgroundColor"))
-            .navigationBarTitle(model.selectedDevice.name.camelCaseToWords())
+            .navigationBarTitle(model.selectedDevice.room.camelCaseToWords() + ":" + model.selectedDevice.name.camelCaseToWords())
             .navigationBarTitleDisplayMode(.inline)
             .foregroundColor(Color("TextColor"))
 
