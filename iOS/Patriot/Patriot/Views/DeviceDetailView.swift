f3978b278ae727b35af524087e66f582b74c75da
Ron <ron@lisles.net>
2022-06-09 06:23:19 -0500

    wip

--- iOS/Patriot/Patriot/Views/DeviceDetailView.swift
+++ iOS/Patriot/Patriot/Views/DeviceDetailView.swift
@@ -7,6 +7,46 @@
 
 import SwiftUI
 
+//struct VerticalSlider: View {
+//    @EnvironmentObject var playData : PlayData
+//    var channelNumber:Int
+//    var sliderHeight:CGFloat
+//
+//    var body: some View {
+//        Slider(
+//            value: self.$playData.flickerDimmerValues[self.channelNumber],
+//            in: 0...255,
+//            step: 5.0
+//        ).rotationEffect(.degrees(-90.0), anchor: .topLeading)
+//        .frame(width: sliderHeight)
+//        .offset(y: sliderHeight)
+//    }
+//}
+
+struct VerticalPercentSlider: View {
+    @State var percent: Int = 0
+    var intProxy: Binding<Double>{
+        Binding<Double>(get: {
+            return Double(percent)
+        }, set: {
+            //rounds the double to an Int
+            print($0.description)
+            percent = Int($0)
+        })
+    }
+    var body: some View {
+        VStack{
+            Text(percent.description)
+            Slider(value: intProxy , in: 0.0...100.0, step: 5.0, onEditingChanged: {_ in
+                print(percent.description)
+            })
+            .rotationEffect(.degrees(-90.0), anchor: .topLeading)
+            .frame(width: 40, height: 200)
+            .offset(y: 200)
+        }
+    }
+}
+
 struct PercentSlider: View {
     @State var percent: Int = 0
     var intProxy: Binding<Double>{
@@ -24,6 +64,7 @@ struct PercentSlider: View {
             Slider(value: intProxy , in: 0.0...100.0, step: 5.0, onEditingChanged: {_ in
                 print(percent.description)
             })
+            .frame(width: 40, height: 200)
         }
     }
 }
@@ -39,7 +80,7 @@ struct DeviceDetailView: View {
             ScrollView {
                 //TODO: add more stuff'
                 VStack {
-                    PercentSlider(percent: model.selectedDevice.percent)
+                    VerticalPercentSlider(percent: model.selectedDevice.percent)
                 }
             }
             .frame(maxWidth: .infinity)
