3dc3d0be1ca6570b9e8c356cc1a41b6323a55827
Ron Lisle <ron@lisles.net>
2022-06-12 15:21:25 -0500

    Fix sleeping (#130)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip

--- iOS/Patriot/Patriot/Views/DeviceDetailView.swift
+++ iOS/Patriot/Patriot/Views/DeviceDetailView.swift
@@ -7,6 +7,70 @@
 
 import SwiftUI
 
+//struct VerticalSlider: View {
+//    @EnvironmentObject var playData : PlayData
+//    var channelNumber:Int
+//    var sliderHeight:CGFloat
+//
+//    var body: some View {
+//        Slider(
+//            value: self.$playData.flickerDimmerValues[self.channelNumber],
+//            in: 0...255,
+//            step: 5.0
+//        ).rotationEffect(.degrees(-90.0), anchor: .topLeading)
+//        .frame(width: sliderHeight)
+//        .offset(y: sliderHeight)
+//    }
+//}
+
+//struct VerticalPercentSlider: View {
+//    @State var percent: Int = 0
+//    var intProxy: Binding<Double>{
+//        Binding<Double>(get: {
+//            return Double(percent)
+//        }, set: {
+//            //rounds the double to an Int
+//            print($0.description)
+//            percent = Int($0)
+//        })
+//    }
+//    var body: some View {
+//        VStack{
+//            Slider(value: intProxy , in: 0.0...100.0, step: 5.0, onEditingChanged: {_ in
+//                print(percent.description)
+//            })
+//            .rotationEffect(.degrees(-90.0), anchor: .topLeading)
+//            .frame(width: 40, height: 200)
+//            .offset(y: 200)
+//            Text(percent.description)
+//        }
+//    }
+//}
+
+struct PercentSlider: View {
+
+    @Binding var percent: Int
+
+    var intProxy: Binding<Double>{
+        Binding<Double>(get: {
+            return Double(percent)
+        }, set: {
+            //rounds the double to an Int
+            print($0.description)
+            percent = Int($0)
+        })
+    }
+    var body: some View {
+        VStack {
+            Text(percent.description)
+            Slider(value: intProxy , in: 0.0...100.0, step: 5.0, onEditingChanged: { value in
+                print(percent.description)
+            })
+            .frame(width: 200, height: 40)
+        }
+    }
+}
+
 struct DeviceDetailView: View {
 
     @EnvironmentObject var model: PatriotModel
@@ -15,13 +79,20 @@ struct DeviceDetailView: View {
         NavigationView {
             ScrollView {
                 //TODO: add more stuff'
-                VStack {
-                    Text("Room: \(model.selectedDevice.room)")
-                    Text(model.selectedDevice.name)
+                VStack(alignment: .leading) {
+                    Text("Brightness")
+                        .font(.title2)
+                        .padding(.top, 40)
+                    PercentSlider(percent: $model.selectedDevice.percent)
                 }
+                .padding(.horizontal, 16)
             }
             .frame(maxWidth: .infinity)
             .background(Color("BackgroundColor"))
+            .navigationBarTitle(model.selectedDevice.room.camelCaseToWords() + ":" + model.selectedDevice.name.camelCaseToWords())
+            .navigationBarTitleDisplayMode(.inline)
+            .foregroundColor(Color("TextColor"))
+
         }
     }
 }
