184d4838b2e9c5302576091456c9ea63fe1e5146
Ron <ron@lisles.net>
2022-06-14 07:04:43 -0500

    wip

--- Photon/Controllers/LeftSlide/src/LeftSlide.ino
+++ Photon/Controllers/LeftSlide/src/LeftSlide.ino
@@ -200,12 +200,12 @@ void handleLivingRoomMotion() {
         Device::setValue("LeftVertical", 50);
 
         // Determine if this is Ron getting up
-        if( partOfDay > SUNSET && sleeping == ASLEEP) {
-            if(Time.hour() > 4 && Time.hour() < 10) {   // Motion after 5:00 is wakeup
-                IoT::mqttPublish("patriot/sleeping", "1");   // AWAKE
-                Device::setValue("sleeping", AWAKE);
-            }
-        }
+//        if( partOfDay > SUNSET && sleeping == ASLEEP) {
+//            if(Time.hour() > 4 && Time.hour() < 10) {   // Motion after 5:00 is wakeup
+//                IoT::mqttPublish("patriot/sleeping", "1");   // AWAKE
+//                Device::setValue("sleeping", AWAKE);
+//            }
+//        }
         livingRoomMotion = true;
 
     } else if(livingRoomMotionChanged == 0) {
