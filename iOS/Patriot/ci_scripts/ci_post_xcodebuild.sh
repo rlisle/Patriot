e0e7b2477d9f17f576e7c61c476012130e8ddb18
Ron <ron@lisles.net>
2022-08-15 09:11:46 -0400

    wip

--- iOS/Patriot/ci_scripts/ci_post_xcodebuild.sh
+++ iOS/Patriot/ci_scripts/ci_post_xcodebuild.sh
@@ -2,6 +2,12 @@
 echo "DEBUG: ci_post_xcodebuild"
 echo $SOURCE_ROOT
 echo $CI
+echo "ls -al"
 ls -al
+echo "ls -al Snapshots"
+ls -al Snapshots
+echo "ls -al /Volumes/workspace/repository/iOS/Patriot/ci_scripts/"
+ls -al /Volumes/workspace/repository/iOS/Patriot/ci_scripts/
+echo "env"
 env
 echo "Done"
