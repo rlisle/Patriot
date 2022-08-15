5a510d22024db82c95f19f2389ae8f2e892faf86
Ron <ron@lisles.net>
2022-08-15 15:11:04 -0400

    wip

--- iOS/Patriot/PatriotTests/AssertSnapshot.swift
+++ iOS/Patriot/PatriotTests/AssertSnapshot.swift
@@ -13,8 +13,6 @@ import Foundation
 import SnapshotTesting
 import XCTest
 
-// Only files in ci_scripts are available to Xcode Cloud tests
-
 public func assertSnapshot<Value, Format>(
     matching value: @autoclosure () throws -> Value,
     as snapshotting: Snapshotting<Value, Format>,
@@ -25,65 +23,49 @@ public func assertSnapshot<Value, Format>(
     testName: String = #function,
     line: UInt = #line
 ) {
-    
-    print("DEBUG: file = \(file)")  // /Users/ronlisle/GitRepos/Patriot/iOS/Patriot/PatriotTests/MenuTests.swift
-
     let isCI = ProcessInfo.processInfo.environment["CI"] == "TRUE"
-//    let envKey = isCI ? "CI_WORKSPACE" : "SOURCE_ROOT"
-//    let sourceRoot = URL(fileURLWithPath: ProcessInfo.processInfo.environment[envKey]!,
-//                         isDirectory: true)
-//
-//    print("DEBUG: sourceRoot = \(sourceRoot)")  // local: file:///Users/ronlisle/GitRepos/Patriot/iOS/Patriot/
-//
-//    let fileUrl = URL(fileURLWithPath: "\(file)", isDirectory: false)
-//    let fileName = fileUrl.deletingPathExtension().lastPathComponent
-//
-//    let absoluteSourceTestPath = fileUrl
-//        .deletingLastPathComponent()
-//        .appendingPathComponent("__Snapshots__")
-//        .appendingPathComponent(fileName)
-//    var components = absoluteSourceTestPath.pathComponents
-//    let sourceRootComponents = sourceRoot.pathComponents
-//    for component in sourceRootComponents {
-//        if components.first == component {  // This fails is component appears multiple times (eg. patriot/Snapshots/patriot)
-//            components = Array(components.dropFirst())
-//        } else {
-//            XCTFail("Test file does not share a prefix path with SOURCE_ROOT")
-//            return
-//        }
-//    }
-//
-//    var snapshotDirectoryUrl = sourceRoot
+    var sourceRoot = URL(fileURLWithPath: ProcessInfo.processInfo.environment["SOURCE_ROOT"]!,
+                         isDirectory: true)
+    if isCI {
+        sourceRoot = URL(fileURLWithPath: "/Volumes/workspace/repository/ci_scripts")
+    }
+
+    let fileUrl = URL(fileURLWithPath: "\(file)", isDirectory: false)
+    let fileName = fileUrl.deletingPathExtension().lastPathComponent
+
+    let absoluteSourceTestPath = fileUrl
+        .deletingLastPathComponent()
+        .appendingPathComponent("__Snapshots__")
+        .appendingPathComponent(fileName)
+    var components = absoluteSourceTestPath.pathComponents
+    let sourceRootComponents = sourceRoot.pathComponents
+    for component in sourceRootComponents {
+        if components.first == component {
+            components = Array(components.dropFirst())
+        } else {
+            XCTFail("Test file does not share a prefix path with SOURCE_ROOT")
+            return
+        }
+    }
+    var snapshotDirectoryUrl = sourceRoot
 //    if isCI {
 //        snapshotDirectoryUrl = snapshotDirectoryUrl.appendingPathComponent("ci_scripts")
 //    }
-//    snapshotDirectoryUrl = snapshotDirectoryUrl.appendingPathComponent("Snapshots")
-//    for component in components {
-//        snapshotDirectoryUrl = snapshotDirectoryUrl.appendingPathComponent(component)
-//    }
-//    print("DEBUG: verifySnapshot directory: \(snapshotDirectoryUrl.path), file: \(file)")
-
-    var snapshotDirectory = "/Users/ronlisle/GitRepos/Patriot/iOS/Patriot/Snapshots/PatriotTests/__Snapshots__/MenuTests"
-    if isCI {
-        snapshotDirectory = "/Volumes/workspace/repository/ci_scripts/Snapshots/PatriotTests/__Snapshots__/MenuTests"
-//        snapshotDirectory = "Snapshots/PatriotTests/__Snapshots__/MenuTests"
+    snapshotDirectoryUrl = snapshotDirectoryUrl.appendingPathComponent("Artifacts")
+    for component in components {
+        snapshotDirectoryUrl = snapshotDirectoryUrl.appendingPathComponent(component)
     }
-    print("DEBUG: verifySnapshot directory: \(snapshotDirectory), file: \(file)")
-    
+
     let failure = verifySnapshot(
         matching: try value(),
         as: snapshotting,
         named: name,
         record: recording,
-//        snapshotDirectory: snapshotDirectoryUrl.path,
-        snapshotDirectory: snapshotDirectory,
+        snapshotDirectory: snapshotDirectoryUrl.path,
         timeout: timeout,
         file: file,
         testName: testName
     )
     guard let message = failure else { return }
-//    XCTFail("\(message) snap: \(snapshotDirectoryUrl)", file: file, line: line)
-    XCTFail("\(message) snap: \(snapshotDirectory)", file: file, line: line)
+    XCTFail("\(message) snap: \(snapshotDirectoryUrl)", file: file, line: line)
 }
-
-
