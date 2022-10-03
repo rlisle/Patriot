df900956199a85eefcae3635612e2fbfa6e859ac
Ron <ron@lisles.net>
2022-10-03 06:49:30 -0500

    wip

--- Photon/Controllers/NCD16LightTest/src/NCD16LightTest.ino
+++ Photon/Controllers/NCD16LightTest/src/NCD16LightTest.ino
@@ -23,7 +23,7 @@
  */
 
 #include <IoT.h>
-//#include <PatriotNCD16Dimmer.h>
+#include <PatriotNCD16Dimmer.h>
 
 #define CONTROLLER_NAME "OfficeTest"
 #define MQTT_BROKER "192.168.50.33"
@@ -32,7 +32,12 @@
 SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
+system_tick_t lastAliveTime = 0;
+
 void setup() {
+    Serial.begin();
+    Serial.println("NCD16LightTest setup");
+    
     WiFi.selectAntenna(ANT_INTERNAL);
     WiFi.useDynamicIP();
     IoT::begin(MQTT_BROKER, CONTROLLER_NAME);
@@ -40,9 +45,23 @@ void setup() {
 }
 
 void createDevices() {
-    Device::add(new NCD16Dimmer(ADDRESS, 0, "SpotLight", "Office", 2));
+//    Device::add(new NCD16Dimmer(ADDRESS, 0, "SpotLight", "Office", 2));
 }
 
 void loop() {
     IoT::loop();
+
+    // Display an alive message every 15 seconds
+    sendAlivePeriodically();
+    
+
+}
+
+void sendAlivePeriodically() {
+    system_tick_t secondsSinceLastAlive = Time.now() - lastAliveTime;
+    if(secondsSinceLastAlive > 15) {
+        lastAliveTime = Time.now();
+        String time = Time.format(Time.now(), "%a %H:%M");
+        Serial.println("Alive "+ time);
+    }
 }
