dcc280258b2ab63f9570added748f847743d3079
Ron <ron@lisles.net>
2022-11-21 08:56:29 -0600

    wip

--- Photon/Controllers/RonTest/src/RonTest.ino
+++ Photon/Controllers/RonTest/src/RonTest.ino
@@ -10,46 +10,21 @@
  */
 
 #include <IoT.h>
-#include <PatriotLight.h>
 
 SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
-//TODO: convert to IPAddress
-byte hueServer[4] = { 192, 168, 50, 21 };
-
-IPAddress myAddress(192,168,50,40);
-IPAddress netmask(255,255,255,0);
-IPAddress gateway(192,168,50,1);
-IPAddress dns(192,168,50,1);
-
-IPAddress mqttAddress(192, 168, 50, 33);
+SerialLogHandler logHandler;
 
 unsigned long lastScan = 0;
 unsigned long scanInterval = 15000;
 
-int blueLED = 0;
-int partOfDay = 0;
 int sleeping = 0;
 
 void setup() {
 //    WiFi.selectAntenna(ANT_EXTERNAL); //TODO: connect external antenna
-    setWifiStaticIP();
-    IoT::begin("192.168.50.33", "RonTest");
-    createDevices();
-}
-
-void setWifiStaticIP() {
-    WiFi.setStaticIP(myAddress, netmask, gateway, dns);
-    WiFi.useStaticIP();
-}
-
-void createDevices() {
-    Device::add(new Light(7, "blueLed", "Office", false, true));
-    
-    // Basic devices allow Alexa to control the name
-    // and can also turn off other activities.
-    Device::add(new Device("partofday", "All"));
+//    WiFi.useDynamicIP();
+    IoT::begin("192.168.50.33", "RonTest", true);
     Device::add(new Device("sleeping", "All"));
 }
 
@@ -57,31 +32,10 @@ void loop() {
 
     IoT::loop();
     
-    handlePartOfDay();  // Do this and sleeping first
     handleSleeping();
     
 //    scanI2Caddresses();
 
-}
-
-void handlePartOfDay() {
-    
-    int partOfDayChanged = Device::getChangedValue("partofday");
-    if( partOfDayChanged != -1 ) {
-
-        Log.info("partOfDay has changed: %d", partOfDayChanged);
-
-        if( partOfDayChanged == SUNRISE ) {
-            Log.info("It is sunrise");
-            setSunriseLights();
-        }
-
-        if( partOfDayChanged == DUSK ) {
-            Log.info("It is dusk");
-            setEveningLights();
-        }
-        partOfDay = partOfDayChanged;
-    }
 }
 
 void handleSleeping() {
@@ -91,55 +45,9 @@ void handleSleeping() {
         
         Log.info("sleeping has changed %d",sleepingChanged);
 
-        // Alexa, Good morning
-        Log.info("Checking for Good Morning: sleeping: %d, partOfDay: %d",sleepingChanged,partOfDay);
-        if( sleepingChanged == AWAKE) {
-            Log.info("It is AWAKE");
-            if(partOfDay > SUNSET || (partOfDay==0 && Time.hour() < 8)) {
-                Log.info("It is morning");
-                setMorningLights();
-            }
-        }
-
-        // Alexa, Bedtime
-        if( sleepingChanged == RETIRING ) {
-            setBedtimeLights();
-        }
-
-        // Alexa, Goodnight
-        if( sleepingChanged == ASLEEP ) {
-            setSleepingLights();
-        }
-        
         sleeping = sleepingChanged;
     }
 }
-
-void setSunriseLights() {
-    Log.info("setSunriseLights");
-    Device::setValue("BlueLED", 0);
-}
-
-void setEveningLights() {
-    Log.info("setEveningLights");
-    Device::setValue("BlueLED", 100);
-}
-
-void setMorningLights() {
-    Log.info("setMorningLights");
-    Device::setValue("BlueLED", 100);
-}
-
-void setBedtimeLights() {
-    Log.info("setBedtimeLights");
-    Device::setValue("BlueLED", 100);
-}
-
-void setSleepingLights() {
-    Log.info("setSleepingLights");
-    Device::setValue("BlueLED", 0);
-}
-
 
 
 // Diagnostic Functions
