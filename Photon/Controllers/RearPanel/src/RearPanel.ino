a06d8ba3b447e6b007b5ce93331e1443d73e18b2
Ron <ron@lisles.net>
2022-11-25 07:02:59 -0600

    wip

--- Photon/Controllers/RearPanel/src/RearPanel.ino
+++ Photon/Controllers/RearPanel/src/RearPanel.ino
@@ -28,47 +28,47 @@ Author: Ron Lisle
  */
 #include <IoT.h>
 #include <PatriotNCD8Light.h>
-//#include <PatriotCurtain.h>
-//#include <PatriotNCD4Switch.h>
-//#include <PatriotNCD4Relay.h>
-//#include <PatriotPIR.h>
-//#include "secrets.h"   // Modify this to include your passwords: HUE_USERID
+#include <PatriotCurtain.h>
+#include <PatriotNCD4Switch.h>
+#include <PatriotNCD4Relay.h>
+#include <PatriotPIR.h>
+#include "secrets.h"   // Modify this to include your passwords: HUE_USERID
 
 #define CONTROLLER_NAME "RearPanel"
 #define MQTT_BROKER "192.168.50.33"
 #define CONNECT_TO_CLOUD true
-//#define OFFICE_MOTION_TIMEOUT 15
+#define OFFICE_MOTION_TIMEOUT 15
 
 #define ADDRESS 1      // PWM board address A0 jumper set
 #define I2CR4IO4 0x20  // 4xRelay+4GPIO address
 
-SYSTEM_THREAD(ENABLED);
-SYSTEM_MODE(SEMI_AUTOMATIC);
+//SYSTEM_THREAD(ENABLED);
+//SYSTEM_MODE(SEMI_AUTOMATIC);
 
-//bool officeMotion = false;
-//long lastOfficeMotion = 0;
+bool officeMotion = false;
+long lastOfficeMotion = 0;
 
-//bool officeDoor = false;
-//bool officeDoorCountdown = false;
-//long lastOfficeDoor = 0;
+bool officeDoor = false;
+bool officeDoorCountdown = false;
+long lastOfficeDoor = 0;
 
 void setup() {
     //WiFi.selectAntenna(ANT_EXTERNAL);
     //WiFi.useDynamicIP();
     IoT::begin(MQTT_BROKER, CONTROLLER_NAME, CONNECT_TO_CLOUD);
-//    createDevices();
+    createDevices();
 }
 
 void createDevices() {
     // I2CIO4R4G5LE board
     // 4 Relays
-//    Device::add(new Curtain(I2CR4IO4, 0, "Curtain", "Office"));     // 2x Relays: 0, 1
+    Device::add(new Curtain(I2CR4IO4, 0, "Curtain", "Office"));     // 2x Relays: 0, 1
     
     // 4 GPIO
-//    Device::add(new NCD4Switch(I2CR4IO4, 0, "OfficeDoor", "Office"));
+    Device::add(new NCD4Switch(I2CR4IO4, 0, "OfficeDoor", "Office"));
     
     // Photon I/O
-//    Device::add(new PIR(A5, "OfficeMotion", "Office", OFFICE_MOTION_TIMEOUT));
+    Device::add(new PIR(A5, "OfficeMotion", "Office", OFFICE_MOTION_TIMEOUT));
 
     // I2CPWM8W80C board
     // 8 Dimmers
@@ -81,52 +81,52 @@ void createDevices() {
     Device::add(new NCD8Light(ADDRESS, 6, "Piano", "Office", 2));
     
     // Pseudo Devices
-//    Device::add(new Device("AnyoneHome", "All", 'X'));
-//    Device::add(new Device("RonHome", "All", 'X'));
-//    Device::add(new Device("ShelleyHome", "All", 'X'));
-//    Device::add(new Device("Nighttime", "All", 'X'));
+    Device::add(new Device("AnyoneHome", "All", 'X'));
+    Device::add(new Device("RonHome", "All", 'X'));
+    Device::add(new Device("ShelleyHome", "All", 'X'));
+    Device::add(new Device("Nighttime", "All", 'X'));
 
     // Checklist Items -  - define for every non-automated checklist item
     
     // Pre-Trip checklist items
-//    Device::add(new Device("startList", "All", 'X'));
-//    Device::add(new Device("checkTires", "All", 'X'));
-//    Device::add(new Device("dumpTanks", "All", 'X'));
-//    Device::add(new Device("fillWater", "All", 'X'));
-//    Device::add(new Device("fuel", "All", 'X'));
-//    Device::add(new Device("fillPropane", "All", 'X'));
-//    Device::add(new Device("checkRoof", "All", 'X'));
-//    Device::add(new Device("checkUnderRV", "All", 'X'));
-//    Device::add(new Device("planRoute", "All", 'X'));
-//
-//    // Depart checklist items
-//    Device::add(new Device("bedSlideIn", "All", 'X'));
-//    Device::add(new Device("LRSlideIn", "All", 'X'));
-//    Device::add(new Device("rampAwningIn", "All", 'X'));
-//    Device::add(new Device("closeRamp", "All", 'X'));
-//    Device::add(new Device("rearAwningIn", "All", 'X'));
-//    Device::add(new Device("latchHandles", "All", 'X'));
-//    Device::add(new Device("frontAwningIn", "All", 'X'));
-//    Device::add(new Device("discPropane", "All", 'X'));
-//    Device::add(new Device("waterHeaterOff", "All", 'X'));
-//    Device::add(new Device("hitchTruck", "All", 'X'));
-//    Device::add(new Device("raiseLG", "All", 'X'));
-//    Device::add(new Device("discPower", "All", 'X'));
-//
-//    // Arrive checklist items
-//    Device::add(new Device("connectPower", "All", 'X'));
-//    Device::add(new Device("lowerLG", "All", 'X'));
-//    Device::add(new Device("bedSlideOut", "All", 'X'));
-//    Device::add(new Device("LRSlidesOut", "All", 'X'));
-//    Device::add(new Device("openRamp", "All", 'X'));
-//    Device::add(new Device("rampAwningOut", "All", 'X'));
-//    Device::add(new Device("stepsDown", "All", 'X'));
-//    Device::add(new Device("rearAwningOut", "All", 'X'));
-//    Device::add(new Device("frontAwningOut", "All", 'X'));
-//    Device::add(new Device("propaneOn", "All", 'X'));
-//    Device::add(new Device("waterHose", "All", 'X'));
-//    Device::add(new Device("waterHeaterOn", "All", 'X'));
-//    Device::add(new Device("sewerHose", "All", 'X'));
+    Device::add(new Device("startList", "All", 'X'));
+    Device::add(new Device("checkTires", "All", 'X'));
+    Device::add(new Device("dumpTanks", "All", 'X'));
+    Device::add(new Device("fillWater", "All", 'X'));
+    Device::add(new Device("fuel", "All", 'X'));
+    Device::add(new Device("fillPropane", "All", 'X'));
+    Device::add(new Device("checkRoof", "All", 'X'));
+    Device::add(new Device("checkUnderRV", "All", 'X'));
+    Device::add(new Device("planRoute", "All", 'X'));
+
+    // Depart checklist items
+    Device::add(new Device("bedSlideIn", "All", 'X'));
+    Device::add(new Device("LRSlideIn", "All", 'X'));
+    Device::add(new Device("rampAwningIn", "All", 'X'));
+    Device::add(new Device("closeRamp", "All", 'X'));
+    Device::add(new Device("rearAwningIn", "All", 'X'));
+    Device::add(new Device("latchHandles", "All", 'X'));
+    Device::add(new Device("frontAwningIn", "All", 'X'));
+    Device::add(new Device("discPropane", "All", 'X'));
+    Device::add(new Device("waterHeaterOff", "All", 'X'));
+    Device::add(new Device("hitchTruck", "All", 'X'));
+    Device::add(new Device("raiseLG", "All", 'X'));
+    Device::add(new Device("discPower", "All", 'X'));
+
+    // Arrive checklist items
+    Device::add(new Device("connectPower", "All", 'X'));
+    Device::add(new Device("lowerLG", "All", 'X'));
+    Device::add(new Device("bedSlideOut", "All", 'X'));
+    Device::add(new Device("LRSlidesOut", "All", 'X'));
+    Device::add(new Device("openRamp", "All", 'X'));
+    Device::add(new Device("rampAwningOut", "All", 'X'));
+    Device::add(new Device("stepsDown", "All", 'X'));
+    Device::add(new Device("rearAwningOut", "All", 'X'));
+    Device::add(new Device("frontAwningOut", "All", 'X'));
+    Device::add(new Device("propaneOn", "All", 'X'));
+    Device::add(new Device("waterHose", "All", 'X'));
+    Device::add(new Device("waterHeaterOn", "All", 'X'));
+    Device::add(new Device("sewerHose", "All", 'X'));
 }
 
 /**
