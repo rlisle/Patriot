2701f88cf855d50e5ef2c8309a3e1135aa797e96
Ron <ron@lisles.net>
2023-08-18 16:45:11 -0500

    embed NCD8Light

--- Photon/Controllers/RearPanel/src/RearPanel.ino
+++ Photon/Controllers/RearPanel/src/RearPanel.ino
@@ -40,14 +40,17 @@ Author: Ron Lisle
  */
 
 #include <IoT.h>
-//#include <PatriotNCD4Switch.h>
-//#include <PatriotNCD8Light.h>
+//#include <PatriotNCD4Switch.h>    - embedded
+//#include <PatriotNCD8Light.h>     - embedded
 //#include <PatriotCurtain.h>
 //#include <PatriotNCD4Relay.h>
 //#include <PatriotPIR.h>
 //#include <PatriotNCD4PIR.h>
 //#include <PatriotAwning.h>
 #include "secrets.h"   // Modify this to include your passwords: HUE_USERID
+#include "math.h"
+
+#define MILLIS_PER_SECOND 1000
 
 #define CONTROLLER_NAME "RearPanel"
 #define MQTT_BROKER "192.168.50.33"
@@ -74,46 +77,14 @@ void setup() {
     //WiFi.useDynamicIP();
     IoT::begin(MQTT_BROKER, CONTROLLER_NAME, MQTT_LOGGING);
     createDevices();
-    
-//    setupNCD4Switch();  // Debugging NCD4Switch
 }
 
-//long    lastPollTime = 0;
-//int     filter = 0;
-//int8_t  boardAddress = 0x20;
-//int8_t  switchBitmap = 0x10;    // Hardcode to 1st input
-//
-//void setupNCD4Switch() {
-//    byte status;
-//    int  retries;
-//
-//    // Only the first device on the I2C link needs to enable it
-//    if(!Wire.isEnabled()) {
-//        Wire.begin();
-//    }
-//
-//    retries = 0;
-//    do {
-//        Wire.beginTransmission(boardAddress);
-//        Wire.write(0x00);                   // Select IO Direction register
-//        Wire.write(0xf0);                   // 0-3 relays, 4-7 in
-//        status = Wire.endTransmission();    // Write 'em, Dano
-//    } while( status != 0 && retries++ < 3);
-//    if(status != 0) {
-//        Log.error("Set IODIR failed");
-//    }
-//    
-//    retries = 0;
-//    do {
-//        Wire.beginTransmission(boardAddress);
-//        Wire.write(0x06);                   // Select pull-up resistor register
-//        Wire.write(0xf0);                   // pull-ups enabled on inputs
-//        status = Wire.endTransmission();
-//    } while( status != 0 && retries++ < 3);
-//    if(status != 0) {
-//        Log.error("Set GPPU failed");
-//    }
-//}
+/**
+Plugin definitions need to appear before being used
+ 
+ NCD4Switch
+ NCD8Light
+ */
 
 /**
  NCD4Switch Defines
@@ -143,6 +114,31 @@ class NCD4Switch : public Device
     void    loop();
 };
 
+class NCD8Light : public Device
+{
+ private:
+    int8_t  _lightNum;                 // Up to 8 lights supported
+    int8_t   _address;                 // Address of board (eg. 0x40)
+
+    int      _dimmingDuration;
+    float    _currentLevel;            // Use for smooth dimming transitions.
+    float    _targetLevel;
+    float    _incrementPerMillisecond;
+    
+    long     _lastUpdateTime;
+
+    int8_t  initializeBoard();
+    void    outputPWM();
+    void    startSmoothDimming();
+    int     scalePWM(float value);
+
+ public:
+    NCD8Light(int8_t address, int8_t lightNum, String name, String room, int8_t duration = 0);
+    void    begin();
+    void    reset();
+    void    setValue(int value);
+    void    loop();
+};
 
 void createDevices() {
     // I2CIO4R4G5LE board
@@ -159,15 +155,15 @@ void createDevices() {
 
     // I2CPWM8W80C board
     // 8 Dimmers
-//    Device::add(new NCD8Light(ADDRESS, 0, "OfficeCeiling", "Office", 2));
-//    Device::add(new NCD8Light(ADDRESS, 1, "Loft", "Office", 2));
-//    Device::add(new NCD8Light(ADDRESS, 2, "RampPorch", "Outside", 2));
-//    Device::add(new NCD8Light(ADDRESS, 3, "RampAwning", "Outside", 2));
-//    Device::add(new NCD8Light(ADDRESS, 4, "RearPorch", "Outside", 2));
-//    Device::add(new NCD8Light(ADDRESS, 5, "RearAwning", "Outside", 2));
-//    Device::add(new NCD8Light(ADDRESS, 6, "Piano", "Office", 2));
-//    //Device::add(new NCD8Light(ADDRESS, 7, "Unused", "Office", 2));
-//
+    Device::add(new NCD8Light(ADDRESS, 0, "OfficeCeiling", "Office", 2));
+    Device::add(new NCD8Light(ADDRESS, 1, "Loft", "Office", 2));
+    Device::add(new NCD8Light(ADDRESS, 2, "RampPorch", "Outside", 2));
+    Device::add(new NCD8Light(ADDRESS, 3, "RampAwning", "Outside", 2));
+    Device::add(new NCD8Light(ADDRESS, 4, "RearPorch", "Outside", 2));
+    Device::add(new NCD8Light(ADDRESS, 5, "RearAwning", "Outside", 2));
+    Device::add(new NCD8Light(ADDRESS, 6, "Piano", "Office", 2));
+    //Device::add(new NCD8Light(ADDRESS, 7, "Unused", "Office", 2));
+
     // Pseudo Devices
     Device::add(new Device("AnyoneHome", "All", 'X'));
     Device::add(new Device("RonHome", "All", 'X'));
@@ -223,6 +219,12 @@ void createDevices() {
 void loop() {
     IoT::loop();
 }
+
+
+/**
+ PLUG-INS
+ */
+
 
 /**
  NCD4Switch
@@ -405,3 +407,209 @@ void NCD4Switch::notify()
 }
 
 
+/**
+ NCD8Light Plugin
+ */
+
+/**
+ * Constructor
+ * @param address is the board address set by jumpers (0-7) 0x01 if low switch set, 0x40 if high
+ * @param lightNum is the channel number on the NCD 8 Light board (0-7)
+ * @param name String name used to address the light.
+ * @param duration Optional seconds value to transition. 0 = immediate, no transition.
+ */
+
+NCD8Light::NCD8Light(int8_t address, int8_t lightNum, String name, String room, int8_t duration)
+                     : Device(name, room)
+{
+    _address = address;
+    _lightNum   = lightNum;
+    _dimmingDuration = duration;
+    _currentLevel = 0.0;
+    _targetLevel = 0.0;
+    _incrementPerMillisecond = 0.0;
+    _lastUpdateTime = 0;
+    _type = 'L';
+}
+
+void NCD8Light::begin() {
+    initializeBoard();
+}
+
+int8_t NCD8Light::initializeBoard() {
+    int status;
+
+    // Only the first light loaded needs to initialize the I2C link
+    if(!Wire.isEnabled()) {
+        Wire.begin();
+    }
+
+    Wire.beginTransmission(_address);   // Seems unnecessary
+    status = Wire.endTransmission();
+
+    if(status == 0) {
+        Wire.beginTransmission(_address);
+        Wire.write(0);          // Control register - No AI, point to reg0 Mode1
+        Wire.write(0);          // Mode1 reg. Osc on, disable AI, subaddrs, allcall
+        Wire.endTransmission();
+
+        Wire.beginTransmission(_address);
+        Wire.write(1);          // Mode2 register
+        Wire.write(0x04);       // Dimming, Not inverted, totem-pole
+        Wire.endTransmission();
+
+        Wire.beginTransmission(_address);
+        Wire.write(0x8c);       // AI + LEDOUT0
+        Wire.write(0xaa);       // LEDOUT0 LEDs 0-3 dimming
+        Wire.write(0xaa);       // LEDOUT1 LEDS 4-7 dimming
+        Wire.endTransmission();
+
+        outputPWM();            // Force light off
+        
+        Log.info("InitializeBoard " + _name + " sucess");
+        
+    } else {
+        Log.error("InitializeBoard " + _name + " FAILED!");
+    }
+
+    return status;
+}
+
+void NCD8Light::reset() {
+    Log.error("Resetting board");
+    Wire.reset();
+    // Do we need any delay here?
+    Wire.begin();
+
+    // Issue PCA9634 SWRST
+    Wire.beginTransmission(_address);
+    Wire.write(0x06);
+    Wire.write(0xa5);
+    Wire.write(0x5a);
+    byte status = Wire.endTransmission();
+    if(status != 0){
+        Log.error("NCD8Light reset write failed for light "+String(_lightNum)+", reseting Wire");
+    }
+    initializeBoard();
+}
+
+/**
+ * Set value
+ * @param value Int 0 to 100.
+ */
+void NCD8Light::setValue(int value) {
+    if( value == _value ) {
+        Log.info("Dimmer " + _name + " setValue " + String(value) + " same so outputPWM without dimming");
+        _currentLevel = _value;
+        outputPWM();
+        return;
+    }
+    
+    _currentLevel = _value;   // hold previous value for transitioning
+    _value = value;
+    _targetLevel = value;
+    if(_dimmingDuration == 0) {
+        _currentLevel = _targetLevel;
+        outputPWM();
+
+    } else {
+        startSmoothDimming();
+    }
+}
+
+/**
+ * Start smooth dimming
+ * Use float _currentValue to smoothly transition
+ * An alternative approach would be to calculate # msecs per step
+ */
+void NCD8Light::startSmoothDimming() {
+    if((int)_currentLevel != _targetLevel){
+        _lastUpdateTime = millis();
+        float delta = _targetLevel - _currentLevel;
+        _incrementPerMillisecond = delta / (float(_dimmingDuration) * 1000);
+        Log.info("Light "+_name+" setting increment to "+String(_incrementPerMillisecond));
+    }
+}
+
+/**
+ * Private Methods
+ */
+
+/**
+ * loop()
+ */
+void NCD8Light::loop()
+{
+    // Is fading transition underway?
+    if(_currentLevel == _targetLevel) {
+        return;
+    }
+    
+    // _currentLevel, _targetLevel, and _incrementPerMillisend are floats for smoother transitioning
+    
+    long loopTime = millis();
+    float millisSinceLastUpdate = (loopTime - _lastUpdateTime);
+    _currentLevel += _incrementPerMillisecond * millisSinceLastUpdate;
+    if(_incrementPerMillisecond > 0.0) {
+        if(_currentLevel > _targetLevel) {
+            _currentLevel = _targetLevel;
+        }
+    } else {
+        if(_currentLevel < _targetLevel) {
+            _currentLevel = _targetLevel;
+        }
+    }
+    _lastUpdateTime = loopTime;
+    outputPWM();
+};
+
+/**
+ * Set the output PWM _currentLevel (0-100)
+ */
+void NCD8Light::outputPWM() {
+    int reg = 2 + _lightNum;
+    
+    int retryCount = 3;
+    byte status;
+    do {
+        Wire.beginTransmission(_address);
+        Wire.write(reg);
+        Wire.write(scalePWM(_currentLevel));
+        status = Wire.endTransmission();
+        retryCount--;
+    } while(status != 0 && retryCount > 0);
+    
+    if(status != 0){
+        reset();
+        retryCount = 5;
+        do {
+            Wire.beginTransmission(_address);
+            Wire.write(reg);
+            Wire.write(scalePWM(_currentLevel));
+            status = Wire.endTransmission();
+            retryCount--;
+        } while(status != 0 && retryCount > 0);
+        
+        if(status != 0) {
+            Log.error("NCD8Light outputPWM write failed twice for light "+String(_lightNum)+", level = "+String(_currentLevel));
+            reset();
+        }
+    }
+}
+
+/**
+ * Convert 0-100 to 0-255 exponential scale
+ * 0 = 0, 100 = 255
+ */
+int NCD8Light::scalePWM(float value) {
+    if (value <= 0) return 0;
+    if (value >= 100) return 255;
+
+    //TODO: This is too extreme. Need to refine algorithm
+    float base = 1.05697667;
+    float pwm = pow(base,value);
+    if (pwm > 255) {
+        pwm = 255;
+    }
+    return (int)pwm;
+}
