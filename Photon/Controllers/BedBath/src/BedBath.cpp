eb196c1f109bd8858d6cdad25314c729697d5312
Ron Lisle <ron@lisles.net>
2024-01-29 06:33:52 -0600

    Photon2b (#169)
    * create BedBath test controller
    
    * Delete binary files

--- Photon/Controllers/BedBath/src/BedBath.cpp
+++ Photon/Controllers/BedBath/src/BedBath.cpp
@@ -4,14 +4,16 @@
  * Date: 1/14/24
  */
 #include "Particle.h"
-#include "IoT.h"
-#include "secrets.h"
+//#include "IoT.h"
+//#include "secrets.h"
 
 #define CONTROLLER_NAME "BedBath"
 #define MQTT_BROKER "192.168.0.33"
 
 SYSTEM_MODE(AUTOMATIC);
 SYSTEM_THREAD(ENABLED);
+
+typedef unsigned long msecs;
 
 // Generally only use 1 or the other
 // Write logs to MQTT if true
@@ -22,12 +24,16 @@ SerialLogHandler logHandler(LOG_LEVEL_INFO);
 
 void setup() {
 //  WiFi.setCredentials(WIFI_SSID, WIFI_PASSWORD);
-  WiFi.selectAntenna(ANT_INTERNAL);
+//  WiFi.selectAntenna(ANT_INTERNAL);
   //WiFi.useDynamicIP();
 
-  IoT::begin(MQTT_BROKER, CONTROLLER_NAME, MQTT_LOGGING);
+//  IoT::begin(MQTT_BROKER, CONTROLLER_NAME, MQTT_LOGGING);
+
+//  Device::add(new Device("Awake", "All", 'P'));
+//  Device::add(new Device("Test", "All", 'P'));
+
 }
 
 void loop() {
-  IoT::loop();
+//  IoT::loop();
 }
