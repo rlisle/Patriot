5d691c7c3508b29ae4e490fbef958b84bf07baad
Ron <ron@lisles.net>
2024-03-11 10:13:27 -0500

    wip

---
+++ Photon/Controllers/FrontPanel/src/ceilingLights.h
@@ -0,0 +1,33 @@
+//------------
+// CeilingLights
+//------------
+int const ceilingTheatrePercent = 15;
+
+void setCeilingLights() {
+    int percent = 0;
+    int current = value("CeilingLights");
+
+    if(is("Cleaning")) {
+        percent = 100;
+
+    } else if(is("LivingRoom")) {
+        percent = value("LivingRoom");
+
+    } else if(is("Theatre")) {
+        percent = ceilingTheatrePercent;
+
+    } else switch(partOfDay()) {
+        case Evening:
+            percent = 100;
+            break;
+        case Retiring:
+        case Asleep:
+        case AwakeEarly:
+        case Morning:
+        case Afternoon:
+            percent = 0;
+    }
+    if(percent != current) {
+        set("CeilingLights", percent);
+    }
+}
